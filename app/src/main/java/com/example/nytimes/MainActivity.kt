47d894ff031892812dfc5d591a9c9abb04b5ebcf
Abdulrahman Mustafa Salah <am3712@fayoum.edu.eg>
2021-03-25 22:38:19 +0200

    add navigation animation

--- app/src/main/java/com/example/nytimes/MainActivity.kt
+++ app/src/main/java/com/example/nytimes/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -18,33 +18,34 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    private val settingsUnavailable = arrayOf(R.id.nav_settings, R.id.nav_details)
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var navController: NavController
-    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toolbar = findViewById(R.id.toolbar)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-
-        val navView: NavigationView = findViewById(R.id.nav_view)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        val navController = navHostFragment.navController
 
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        navView.setupWithNavController(navController)
-
-        //setupWithNavController(navController, appBarConfiguration)
+        setupNavigationMenu(navController)
     }
+
+
+    private fun setupNavigationMenu(navController: NavController) {
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView.setupWithNavController(navController)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -57,19 +58,16 @@ class MainActivity : AppCompatActivity() {
         // Handle item selection
         return when (item.itemId) {
             R.id.action_settings -> {
-                navController.navigate(PopularFragmentDirections.actionNavPopularToNavSettings())
+                findNavController(R.id.nav_host_fragment).navigate(PopularFragmentDirections.actionNavPopularToNavSettings())
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        //handleNavigation()
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
+
 }
