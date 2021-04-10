25f4e2f6d18301374ab18af17aaca3e98e6f784e
Abdulrahman Mustafa Salah <am3712@fayoum.edu.eg>
2021-04-11 00:43:38 +0200

    fix onOnBackPressed in settings fragmnet

--- app/src/main/java/com/example/nytimes/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/nytimes/ui/settings/SettingsFragment.kt
@@ -1,12 +1,12 @@
 package com.example.nytimes.ui.settings
 
-import android.content.Intent
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
-import androidx.activity.addCallback
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.NavHostFragment
 import androidx.preference.PreferenceFragmentCompat
-import com.example.nytimes.MainActivity
 import com.example.nytimes.R
 
 
@@ -15,22 +15,23 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-        requireActivity().onBackPressedDispatcher.addCallback(this) {
-            //sol 1
-//            findNavController()
-//                .navigate(
-//                    R.id.nav_graph,
-//                    null,
-//                    NavOptions.Builder().setPopUpTo(R.id.nav_graph, true).build()
-//                )
-            //sol 2
-//            findNavController().graph.clear()
-//            findNavController().navigate(R.id.nav_graph)
+    }
 
-            val intent = Intent(requireContext(), MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(
+            true // default to enabled
+        ) {
+            override fun handleOnBackPressed() {
+                NavHostFragment.findNavController(this@SettingsFragment).navigateUp();
+            }
         }
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this,  // LifecycleOwner
+            callback
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
